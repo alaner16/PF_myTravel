2c306685a739f1e3970887e1f4a4fc90a21d1944
ALAN ERVEY RIVERA BOJORQUEZ <alan_rivera_16@hotmail.com>
2017-10-19 05:42:48 -0600

    faltan mas cosas como mensajes, pero hay que dormir un rato

--- src/pages/about/about.ts
+++ src/pages/about/about.ts
@@ -7,8 +7,40 @@ import { NavController } from 'ionic-angular';
 })
 export class AboutPage {
 
+  items: string[];
   constructor(public navCtrl: NavController) {
 
+  }
+  initializeItems() {
+    this.items = [
+      'Hotel Fiesta Inn',
+      'Hotel Ibis',
+      'Hotel Best Wenster',
+      'Motel La Pasadita',
+      'Motel La Oficina',
+      'Bar Ricos',
+      'Bar Chapo',
+      'Restaurante Saul',
+      'Restaurante Maviri',
+      'Los Nachos',
+      'Don Pancho',
+      'Tortas Plebes'
+    ];
+  }
+
+  getItems(ev: any) {
+    // Reset items back to all of the items
+    this.initializeItems();
+
+    // set val to the value of the searchbar
+    let val = ev.target.value;
+
+    // if the value is an empty string don't filter the items
+    if (val && val.trim() != '') {
+      this.items = this.items.filter((item) => {
+        return (item.toLowerCase().indexOf(val.toLowerCase()) > -1);
+      })
+    }
   }
 
 }
